640fcd9538888aee535121e9c390a48d280f88ae
Sakthivelamari <138536342+SakthivelmariArumugam@users.noreply.github.com>
2024-03-19 13:51:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+
+function function1()
+{
+    var file= prompt("main.html");
+    document.getElementById('mainframe1').src=file;
+}
+function function2()
+{
+    var file= prompt("Portfolio.html");
+    document.getElementById('mainframe1').src=file;
+}
+function function3()
+{
+    var file= prompt("service.html");
+    document.getElementById('mainframe1').src=file;
+}
+function function4()
+{
+    var file= prompt("Blog.html");
+    document.getElementById('mainframe1').src=file;
+}
+function function5()
+{
+    var file= prompt("contact.html");
+    document.getElementById('mainframe1').src=file;
+}
+function function6()
+{
+    var file= prompt("Resume.html");
+    document.getElementById('mainframe1').src=file;
+}
